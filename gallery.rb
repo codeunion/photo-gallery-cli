22b55ce0a21636ebb6ca6bb9a7bc9fef01212501
Tanner Welsh <tannerwelsh@gmail.com>
2014-09-25 11:06:02 -0400

    Generate an image tag as a string

--- gallery.rb
+++ gallery.rb
@@ -1,5 +1,9 @@
 # We're only concerned with the first argument for now
 photo_file = ARGV[0]
 
-# Write the absolute path to the file to STDOUT
-puts File.absolute_path(photo_file)
+# Use the absolute path of the photo file
+absolute_path_to_photo = File.absolute_path(photo_file)
+
+# Write an HTML <img> tag with the photo file provided
+# as the value for the src attribute
+puts "<img src=\"#{absolute_path_to_photo}\">"
