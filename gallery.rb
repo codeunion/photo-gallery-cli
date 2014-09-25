36828769737a41a92eb6688894a7080d368d10c2
Tanner Welsh <tannerwelsh@gmail.com>
2014-09-25 13:36:06 -0400

    Extract export-related functions to GalleryExporter module
    Limit the public interface of PhotoGallery to just a few methods:
    - #export
    - #photos
    - #to_html

--- gallery.rb
+++ gallery.rb
@@ -7,12 +7,16 @@
 # $ ruby gallery.rb image.jpg pic.png funny.gif
 #
 
-require 'fileutils'
-
 require_relative './lib/html_generator.rb'
+require_relative './lib/gallery_exporter.rb'
 
 class PhotoGallery
   include HTMLGenerator
+  include GalleryExporter
+
+  # We don't want these utility methods to be part of the public interface
+  private *HTMLGenerator.instance_methods
+  private *GalleryExporter.instance_methods
 
   GALLERY_CSS = <<-CSS
     img {
@@ -25,8 +29,6 @@ class PhotoGallery
       box-shadow: 3px 3px 5px #ccc;
     }
   CSS
-
-  attr_reader :original_photo_files
 
   def initialize(photos)
     @original_photo_files = photos
@@ -61,47 +63,7 @@ class PhotoGallery
 
 private
 
-  def build_directory_struture(target_directory)
-    self.export_directory = target_directory
-    self.img_directory = File.join(export_directory, 'imgs')
-  end
-
-  def copy_photos
-    original_photo_files.each do |photo_file|
-      # Copy each original photo file into the new image directory
-      FileUtils.cp(photo_file, img_directory)
-    end
-  end
-
-  def copied_photos
-    # The copied photo files will live in their own image directory
-    @copied_photos ||= img_directory && Dir[ File.join(img_directory, '*') ]
-  end
-
-  def export_filepath
-    # The file where the generated HTML will be saved
-    File.join(export_directory, 'gallery.html')
-  end
-
-  attr_reader :export_directory
-
-  def export_directory=(directory)
-    # If the directory where the gallery will be exported to does not already
-    # exist, we need to create it.
-    Dir.mkdir(directory) unless Dir.exists?(directory)
-
-    @export_directory = directory
-  end
-
-  attr_reader :img_directory
-
-  def img_directory=(directory)
-    # If the directory where the images will be stored to does not already
-    # exist, we need to create it.
-    Dir.mkdir(directory) unless Dir.exists?(directory)
-
-    @img_directory = directory
-  end
+  attr_reader :original_photo_files
 
   def default_directory_path
     # The default save directory is called `public/` and lives in the root path
