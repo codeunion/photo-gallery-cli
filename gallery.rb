9d0930a3187db5c8d725bc1421d906a13bb34d45
Jesse Farmer <jesse@codeunion.io>
2014-10-05 20:57:48 -0700

    Proceduralize example

--- gallery.rb
+++ gallery.rb
@@ -7,101 +7,83 @@
 # $ ruby gallery.rb image.jpg pic.png funny.gif
 #
 
-module HTMLGenerator
-  def html_template(*content)
-    # Use the heredoc syntax to create a multi-line string for defining the
-    # template of the html page to be generated
-    layout = <<-HTML
+def html_template(content)
+  layout = <<-HTML
 <!DOCTYPE html>
 <html>
 <head>
   <title>My Gallery</title>
   <style type="text/css" media="screen">
-    #{ _style }
+    body {
+      font-family: "Helvetica", sans-serif;
+    }
+
+    .container {
+      margin: 0 auto;
+      width: 720px;
+    }
+
+    img {
+      width: 200px;
+      height: 200px;
+      padding: 0px;
+      margin-right: 24px;
+      border: 3px solid #ccc;
+      border-radius: 2px;
+      box-shadow: 3px 3px 5px #ccc;
+    }
   </style>
 </head>
 <body>
-  <div class="container">
-    <h1>My Gallery</h1>
-    #{ content.join("\n    ") }
-  </div>
+  <h1>My Gallery</h1>
+  #{content}
 </body>
 </html>
-    HTML
+HTML
 
-    layout
-  end
-
-  def img_tag(source_file)
-    # Write an HTML <img> tag with the photo file provided
-    # as the value for the src attribute
-    "<img src=\"#{source_file}\">"
-  end
-
-  def _style
-    <<-CSS
-* {
-  font-family: "Helvetica", sans-serif;
-}
-
-.container {
-  margin: 0 auto;
-  width: 720px;
-}
-
-img {
-  width: 200px;
-  height: 200px;
-  padding: 0px;
-  margin-right: 24px;
-  border: 3px solid #ccc;
-  border-radius: 2px;
-  box-shadow: 3px 3px 5px #ccc;
-}
-    CSS
-  end
+  layout
 end
 
-class PhotoGallery
-  include HTMLGenerator
-
-  attr_reader :photos
-
-  def initialize(photos)
-    @photos = photos
-  end
-
-  def to_html
-    # Generate an array of <img> tags
-    images = photos.map { |photo| img_tag(photo) }
-
-    # Return the full HTML template with the images in place
-    html_template( images )
-  end
+def img_tag(source_file)
+  "<img src=\"#{source_file}\">"
 end
 
-# Only execute the following code if the program being run is this same file,
-# i.e. this will only run if you enter the command
-#
-#   $ ruby gallery.rb some-photo.jpg
-#
-# in the command line.
-#
-# This way, if other programs want to use the utility functions declared
-# in this file, they can `require` the file _without_ actually executing
-# the code below, which expects an argument and writes to STDOUT.
+def img_tag_list(source_files)
+  img_tags = []
+
+  source_files.each do |source_file|
+    img_tags.push(img_tag(source_file))
+  end
+
+  img_tags
+end
+
+def absolute_path_list(paths)
+  absolute_paths = []
+
+  paths.each do |path|
+    absolute_paths.push(File.absolute_path(path))
+  end
+
+  absolute_paths
+end
+
 if __FILE__ == $PROGRAM_NAME
-  # Expect a list of photo files
-  photo_files = ARGV
+  # Expect a list of photo files as command line arguments.
+  # Remember: ARGV will be an Array of Strings - one per argument.
+  photo_paths = ARGV
 
-  # Create an array of absolute paths to each photo
-  absolute_paths_to_photos = photo_files.map { |file| File.absolute_path(file) }
+  # Create an Array of absolute paths to each photo
+  absolute_paths_to_photos = absolute_path_list(photo_paths)
 
-  # Build a new photo gallery
-  gallery = PhotoGallery.new(absolute_paths_to_photos)
+  # Create an Array of <img> tag Strings
+  image_tags = img_tag_list(absolute_paths_to_photos)
+
+  # Join the Array of <img> tags into a single String.
+  # See: http://www.ruby-doc.org/core-2.1.3/Array.html#method-i-join
+  content = image_tags.join("\n")
 
   # Write a full HTML page to STDOUT with the list of <img> tags provided as the
   # content of the page
-  puts gallery.to_html
+  puts html_template(content)
 end
-
